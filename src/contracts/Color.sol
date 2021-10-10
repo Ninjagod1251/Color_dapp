f8f2dd842dc0ae4f098e4f1b8ce9d5e3df46b26d
Ninjagod1251 <chriscao1251@gmail.com>
2021-10-10 13:30:41 -0700

    first commit

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -1,20 +1,24 @@
-pragma solidity 0.5.0;
-
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
 import "./ERC721Full.sol";
 
 contract Color is ERC721Full {
   string[] public colors;
-  mapping(string => bool) _colorExists;
+  mapping(string => bool) _colorExist;
 
+
+   
   constructor() ERC721Full("Color", "COLOR") public {
   }
 
-  // E.G. color = "#FFFFFF"
-  function mint(string memory _color) public {
-    require(!_colorExists[_color]);
-    uint _id = colors.push(_color);
-    _mint(msg.sender, _id);
-    _colorExists[_color] = true;
-  }
+  function mint(string memory _color) public{
+    //require = true break and stop
+    //require false continue 
+    require(!_colorExist(_color));
 
+    uint _id = color.push(_color);
+    _mint(msg.sender, _id);
+    _colorExist[_color] = true;
+
+  }
 }
